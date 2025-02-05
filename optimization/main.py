8e3f1d42e560f6b9376eb945238a9c194ffbfb74
JanikPinter <janik.pinter@kit.edu>
2025-02-05 11:22:56 +0100

    Minor changes

--- optimization/main.py
+++ optimization/main.py
@@ -9,14 +9,14 @@ from experiment_tracking import start_experiment, log_data, end_experiment, log_
 
 def main():
     # Example 1: Normal Distribution
-    fc_folder = 'data/parametric_forecasts/normal_dist_forecast_2024-10-31/'
-    params_path = 'data/parameters/params_normal_dist.json'
-    timeframe = ['2017-05-18 06:00:00', '2017-05-19 05:00:00']
+    # fc_folder = 'data/parametric_forecasts/normal_dist_forecast_2024-10-31/'
+    # params_path = 'data/parameters/params_normal_dist.json'
+    # timeframe = ['2017-05-18 06:00:00', '2017-05-19 05:00:00']
 
     # Example 2: Sum of two logistic functions (Recreate paper results)
-    #fc_folder = 'data/parametric_forecasts/s2l_dist_forecast_2024-10-02/' 
-    #params_path = 'data/parameters/params_case2.json'
-    #timeframe = ['2017-05-18 06:00:00', '2017-05-19 05:00:00']  # Forecasted nighttime PDFs can be too tight for plotting or solving the optimization model (See limitations section in the paper).
+    fc_folder = 'data/parametric_forecasts/s2l_dist_forecast_2024-10-02/' 
+    params_path = 'data/parameters/params_case2.json'
+    timeframe = ['2017-05-18 06:00:00', '2017-05-19 05:00:00']  # Forecasted nighttime PDFs can be too tight for plotting or solving the optimization model (See limitations section in the paper).
                                                                 # Simple solution: Either choose a different date, adjust the timeframe to exclude nighttime (e.g. 06:00-23:00), or use gaussian distribution.
                                                                 # Better solution: Redo the forecasts with additional constraints, or approximate nighttime PDFs with a different distribution (GMM looks promising).
 
